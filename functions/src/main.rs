d5bbd40fa1b8376fc58822af3b94ba73436765e4
datsuns <the.skeleton7@gmail.com>
2022-06-23 12:49:47 +0900

    add function with return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
     another_function(5);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    let z = function_with_return_value();
+    println!("The value of y is {}", y);
+    println!("return value is {}", z);
 }
 
 fn another_function(x: i32) {
     println!("The value of x is {}", x);
+}
+
+fn function_with_return_value() -> i32 {
+    5
 }
