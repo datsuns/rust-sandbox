6a24333463f722446444cafb319bcf6dd13bb932
bando <dummy@dummy.com>
2022-07-20 08:14:58 +0900

    add rectangle (1st ver.)

---
+++ rectangles/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+    println!(
+        "The area of rectrangle is {} squrare pixels.",
+        area(width1, height1)
+        );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
