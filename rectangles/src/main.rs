57108d1d006d18dfd899fe51f6a3f5c4b9f21757
datsuns <the.skeleton7@gmail.com>
2022-07-26 08:25:50 +0900

    add method and associated function for Rectangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     area_by_normal_variable();
     area_by_touple();
     area_by_structure();
+    area_by_method();
 }
 
 // 独自の方に有用な振る舞いを追加できる
@@ -9,6 +10,44 @@ fn main() {
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn area_by_method() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let rect4 = Rectangle::square(3);
+    println!(
+        "The area of the rectangle is {} square pixesls.",
+        rect1.area()
+    );
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("rect4 is {:#?}", rect4);
 }
 
 fn area_by_structure() {
